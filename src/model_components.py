637dc0c62181af0ea319b7cab2a392e3604b008f
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-04-18 11:39:14 -0700

    Remove bias from fully-connected layer in patch class

--- src/model_components.py
+++ src/model_components.py
@@ -7,7 +7,7 @@ class Patch(nn.Module):
         super(Patch, self).__init__()
         self.patch_size = patch_size
         self.latent_space_dim = latent_space_dim
-        self.fc1 = None # placeholder
+        self.fc1 = nn.Linear(self.patch_size * self.patch_size, self.latent_space_dim, bias=False)
 
     def forward(self, x):
         B, C, H, W = x.shape
@@ -15,11 +15,6 @@ class Patch(nn.Module):
         num_patches = (H // self.patch_size) * (W // self.patch_size)
 
         patches = x.unfold(2, self.patch_size, self.patch_size).unfold(3, self.patch_size, self.patch_size)
-        patches = patches.contiguous().view(B, C, num_patches, self.patch_size * self.patch_size)
-
-        patches = patches.view(B, num_patches, -1)
-
-        if self.fc1 is None:
-            self.fc1 = nn.Linear(C * self.patch_size * self.patch_size, self.latent_space_dim)
+        patches = patches.contiguous().view(B, num_patches, C * self.patch_size * self.patch_size)
 
         return self.fc1(patches)

11c0d15fbf788c75ed984dae801c8c2bd16bda9e
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-04-27 10:10:16 -0700

    Change patch embedding from embedding table to fc layer

--- src/model.py
+++ src/model.py
@@ -2,15 +2,18 @@ import torch
 from torch import nn
 
 class VisionTransformer(nn.Module):
-    def __init__(self, image_width: int, image_height: int, patch_size: int, latent_space_dim: int, num_classes: int):
+    def __init__(
+        self, image_width: int, image_height: int, channel_size: int,
+        patch_size: int, latent_space_dim: int, num_classes: int
+    ):
         super(VisionTransformer, self).__init__()
         self.patch_size = patch_size
         self.latent_space_dim = latent_space_dim
         self.num_patches = (image_width * image_height) // (self.patch_size * self.patch_size)
         self.num_classes = num_classes
-        self.pixel_embedding_table = nn.Embedding(
-            self.patch_size * self.patch_size, 
-            self.latent_space_dim
+        self.patch_embedding_layer = nn.Linear(
+            channel_size * self.patch_size * self.patch_size,  
+            self.latent_space_dim  
         )
         self.positional_embedding_table = nn.Parameter(
             self.num_patches,
@@ -23,7 +26,7 @@ class VisionTransformer(nn.Module):
         patches = x.unfold(2, self.patch_size, self.patch_size).unfold(3, self.patch_size, self.patch_size)
         patches = patches.contiguous().view(B, self.num_patches, C * self.patch_size * self.patch_size)
 
-        pixel_embedding = self.pixel_embedding_table(patches)
+        patch_embedding = self.patch_embedding_layer(patches)
         positional_embedding = self.positional_embedding_table(
             torch.arange(self.num_patches, device=patches.device)
         )

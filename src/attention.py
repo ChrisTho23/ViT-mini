514200cbb125defa17b18e972f8f2b7d837c62f7
Jacob Brandauer <jacobbrandauer@googlemail.com>
2024-04-28 20:18:06 +0200

    Fixed typos and layer norm

--- src/attention.py
+++ src/attention.py
@@ -9,12 +9,12 @@ class self_attention_layer(torch.nn.Module):
         self.keys_layer = torch.nn.Linear(dim_in, dim_out, bias = False)
         self.queries_layer = torch.nn.Linear(dim_in, dim_out, bias = False)
         self.values_layer = torch.nn.Linear(dim_in, dim_out, bias=False)
-        self.softmax_layer = torch.nn.Linear(dim=-1)
+        self.softmax_layer = torch.nn.Softmax(dim=-1)
     def get_att_scores(self, keys, queries):
         """
-        Implements computation of attention scores
-        :param keys: keys [B, C, D]
-        : param queries: [B, C, D]
+        Implements computation of attention scores:
+        :params keys: keys [B, N, D]
+        :params queries: [B, N, D]
         """
         return self.softmax_layer(torch.einsum('bid, bjd->bij', queries, keys))
 
@@ -23,7 +23,7 @@ class self_attention_layer(torch.nn.Module):
         """
         Implements forward pass of self-attention layer
         params:
-        x: [B, C, D] (B: batch size, C: context-length, D: latent space dim.)
+        x: [B, N, D] (B: batch size, N: context-length, D: latent space dim.)
         """
         B, C, D = x.shape
 
@@ -35,19 +35,22 @@ class self_attention_layer(torch.nn.Module):
         # attention mechanism
         att_scores = self.get_att_scores(keys, queries)
 
-        return torch.einsum('bjd, bij->bid', att_scores, att_scores) # shape: [B, C, D]
+        return torch.einsum('bjd, bij->bid', values, att_scores) # shape: [B, N, D]
 
 
 class multi_head_self_attention_layer(torch.nn.Module):
-    def __init__(self, dim_in, dim_out, num_heads):
+    def __init__(self, dim_in, dim_out, num_heads, context_length):
         super(multi_head_self_attention_layer, self).__init__()
         self.dim_in = dim_in
         self.dim_out = dim_out
         self.num_heads = num_heads
         self.d_head = dim_out // num_heads
+        self.context_length = context_length
         self.output_projection = torch.nn.Linear(dim_out, dim_out)
+        self.layer_norm =torch.nn.LayerNorm({self.context_length, self.dim_in})
 
         self.attention_heads = [self_attention_layer(dim_in, self.d_head) for i in self.num_heads]
     def forward(self, x):
-        x = torch.concat([self_att(x) for self_att in self.attention_heads], axis=-1) # concatenate along data dim
-        return self.output_projection(x)
+        x = self.layer_norm(x)
+        x_prime = torch.concat([self_att(x) for self_att in self.attention_heads], axis=-1) # concatenate along data dim
+        return self.output_projection(x_prime)+x

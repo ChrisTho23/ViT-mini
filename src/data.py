879f68e7baff8d4e0640830bf1d27571033c0211
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-05-02 22:45:57 -0700

    remove test since moved to test directory

--- src/data.py
+++ src/data.py
@@ -39,22 +39,3 @@ def load_cifar10(target_directory, download, batch_size: int = 64) -> torch.util
 
     return train_loader, test_loader
 
-if __name__ == "__main__":
-    args = get_args()
-
-    # load CIFAR-10 dataset
-    try:
-        train_loader, test_loader = load_cifar10(DATA_DIR, download=args.download, batch_size=TRAINING["batch_size"])
-    except Exception as e:
-        logging.error(f"Error loading CIFAR-10 dataset: {e}")
-        logging.error("Please run the script with the flag '--download True' to download the dataset")
-
-    # get some random training images
-    dataiter = iter(train_loader)
-    images, labels = next(dataiter)
-
-    # show images
-    logging.info("Displaying some random images from the CIFAR-10 dataset")
-    logging.info(f"Each image has dimensions: {images.shape[1:4]}")
-    display_image(torchvision.utils.make_grid(images))
-

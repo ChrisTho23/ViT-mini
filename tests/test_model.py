b06395eebfceaceaf3d1080bbb99a85dc18b007c
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-05-02 23:29:33 -0700

    Adapted test

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,7 +3,7 @@ import logging
 
 from src.model import VisionTransformer
 from src.data import load_cifar10
-from src.config import TRAINING, DATA, DATA_DIR
+from src.config import TRAINING, DATA, MODEL, DATA_DIR
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
@@ -21,8 +21,10 @@ def model(request):
     """Fixture to initialize the Vision Transformer model."""
     vit_model = VisionTransformer(
         image_width=DATA["image_width"], image_height=DATA["image_height"],
-        channel_size=DATA["channels"], patch_size=DATA["patch_size"],
-        latent_space_dim=DATA["latent_space_dim"]
+        channel_size=DATA["channel_size"], patch_size=DATA["patch_size"],
+        latent_space_dim=MODEL["latent_space_dim"], dim_ff=MODEL["dim_ff"],
+        num_heads=MODEL["num_heads"], depth=MODEL["depth"], 
+        num_classes=DATA["num_classes"]
     )
     request.cls.model = vit_model
 
@@ -34,9 +36,8 @@ class TestVisionTransformer:
         """Test that embedding shape is as expected after a forward pass."""
         data_iter = iter(self.train_loader)
         images, _ = next(data_iter)
-        embedding = self.model(images)
-        logging.info(f"Output embedding shape: {embedding.shape}")
+        logits, loss = self.model(images)
+        logging.info(f"Output embedding shape: {logits.shape}, loss is {loss}")
 
-        num_patches = (DATA["image_height"] * DATA["image_width"]) // (DATA["patch_size"]**2)
-        expected_shape = (TRAINING["batch_size"], num_patches, DATA["latent_space_dim"])
-        assert embedding.shape == expected_shape, f"Expected shape {expected_shape}, but got {embedding.shape}"
+        expected_shape = (TRAINING["batch_size"], DATA["num_classes"])
+        assert logits.shape == expected_shape, f"Expected shape {expected_shape}, but got {logits.shape}"

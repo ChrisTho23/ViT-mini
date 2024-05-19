996b822cd315ff2caab72b57b5a31761e815d586
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-05-18 23:25:53 -0700

    Remove patch class since not needed

--- src/model_components.py
+++ src/model_components.py
@@ -4,18 +4,6 @@ import numpy as np
 
 from src.attention import MultiHeadSelfAttentionLayer
 
-class Patch(nn.Module): # do we need this?
-    def __init__(self, patch_size: int, latent_space_dim: int):
-        super(Patch, self).__init__()
-        self.patch_size = patch_size
-        self.latent_space_dim = latent_space_dim
-        self.fc1 = nn.Linear(self.patch_size * self.patch_size, self.latent_space_dim, bias=False)
-    def forward(self, x):
-        B, C, H, W = x.shape
-        num_patches = (H // self.patch_size) * (W // self.patch_size)
-        patches = x.unfold(2, self.patch_size, self.patch_size).unfold(3, self.patch_size, self.patch_size)
-        patches = patches.contiguous().view(B, num_patches, C * self.patch_size * self.patch_size)
-
 class FeedForward(nn.Module):
     def __init__(self, dim_in: int, dim_ff: int, dim_out: int , dtype=float):
         super(FeedForward, self).__init__()

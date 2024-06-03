1d792d283cdd68d1141938ebe3817a59dc71f666
ChrisTho23 <christophe.thomassin23@gmail.com>
2024-06-03 00:37:48 +0000

    Adjust learning rate and add patience parameter

--- src/config.py
+++ src/config.py
@@ -4,6 +4,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = BASE_DIR / 'data'
 MODEL_DIR = BASE_DIR / 'model'
 
+SEED = 42
+
 DATA = {
     "image_width": 32,
     "image_height": 32,
@@ -15,8 +17,10 @@ DATA = {
 TRAINING = {
     "batch_size": 64,
     "num_epochs": 7, # 7 or 14 
-    "learning_rate": 5e-6, # 2e-4 - 8e-4
+    "learning_rate": 8e-6, # 2e-4 - 8e-4
     "weight_decay": 0.1,
+    "val_size": 0.1,
+    "patience": 2,
 }
 
 MODEL = {
